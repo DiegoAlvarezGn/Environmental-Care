113b298f59fd8470003a80e80c739267eea222cc
DiegoAlvarezGn <alvarezguzman.dh@gmail.com>
2023-05-25 23:08:52 -0600

    router edited

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
-  HashRouter,
   RouterProvider,
 } from "react-router-dom";
 import Main from './routes/Main';
@@ -31,9 +30,7 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <RouterProvider router={router} />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
